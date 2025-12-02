b62e2f060dc908d2381190031b32cf9b1cc82005
Sudhanshukunwar <136906592+Sudhanshukunwar@users.noreply.github.com>
2025-12-02 10:52:25 +0000

    container config done

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId = 14434
+let accountEmail ="sudh@gmail.com"
+var accountpassword = "12345"
+accountCity = "Mumbai"
+
+
+console.table([accountEmail,accountId,accountpassword,accountCity]);
